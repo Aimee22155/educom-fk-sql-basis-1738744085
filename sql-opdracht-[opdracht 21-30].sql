97fb771432626b191f802ff9a217f9c471958bd8
Aimee22155 <heusschenaimee@gmail.com>
2025-02-06 16:03:32 +0100

    Create sql-opdracht-[opdracht 21-30].sql

---
+++ sql-opdracht-[opdracht 21-30].sql
@@ -0,0 +1,24 @@
+--opdracht 21--
+SELECT City, CompanyName, ContactName
+FROM customers
+WHERE Country LIKE '%L%'
+ORDER BY ContactName;
+
+--opdracht 22--
+
+--opdracht 23--
+
+--opdracht 24--
+
+--opdracht 25--
+
+--opdracht 26--
+
+--opdracht 27--
+
+--opdracht 28--
+
+--opdracht 29--
+
+--opdracht 30--
+
