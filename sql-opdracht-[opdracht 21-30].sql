de140c137c15b65b429faa717d0be8d9b88894f6
Aimee22155 <heusschenaimee@gmail.com>
2025-02-06 16:37:54 +0100

    Update sql-opdracht-[opdracht 21-30].sql

--- sql-opdracht-[opdracht 21-30].sql
+++ sql-opdracht-[opdracht 21-30].sql
@@ -5,14 +5,34 @@ WHERE Country LIKE '%L%'
 ORDER BY ContactName;
 
 --opdracht 22--
+SELECT FirstName, LastName, BirthDate
+FROM employees
+WHERE BirthDate BETWEEN '1950-01-01' AND '1959-12-31';
 
 --opdracht 23--
+SELECT FirstName, LastName, YEAR(BirthDate) AS BirthYear
+FROM employees
 
 --opdracht 24--
+SELECT OrderID, COUNT(OrderID) AS NumberofOrders
+FROM OrderDetails
+GROUP BY OrderID
+ORDER BY NumberofOrders DESC;
 
 --opdracht 25--
+SELECT
+    products.SupplierID,
+    products.ProductName,
+    suppliers.CompanyName
+FROM products
+JOIN suppliers ON products.SupplierID = suppliers.SupplierID
+WHERE suppliers.CompanyName IN('Exotic Liquids', 'Specialty Biscuits, Ltd.','Escargots Nouveaux')
+ORDER BY products.SupplierID;
 
 --opdracht 26--
+SELECT ShipPostalCode, OrderID, OrderDate, RequiredDate, ShippedDate, ShipAddress
+FROM orders
+WHERE ShipPostalCode LIKE '98124%';
 
 --opdracht 27--
 
